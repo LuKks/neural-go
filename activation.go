741c7a8960f14c852da8d5f9c3cfdd4f68a8fa93
LuKks <lucasbarrena@hotmail.com>
2019-12-04 17:14:31 -0300

    improving docs

--- activation.go
+++ activation.go
@@ -58,9 +58,7 @@ func ReluBackward(activation float64) float64 {
 
 // ActivationSet is a forward and backward fn with its range
 type ActivationSet struct {
-	// Forward fn
 	Forward ForwardFn
-	// Backward fn
 	Backward BackwardFn
 	// Range of the activation
 	Ranges []float64
